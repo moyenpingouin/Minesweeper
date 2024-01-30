7a557e6b86ec67067e1979fb2d5496495a105a2e
AIphaO6 <re.kaelbel@gmail.com>
2024-01-30 11:19:59 +0100

    remplir

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 #import
 import os
-import pygame
+#import pygame
+from random import randint
 from classes import *
 #Variables
 jeu_revele=[]
@@ -37,3 +38,26 @@ print(fonction())
 
 
 #code
+def remplir(l:int,h:int,X:int,Y:int,max_mine:int):
+    """l:longeur du tableau
+    h:hauteur du tableau
+    X,Y: coordonnees du point d'initialisation
+    OUT:liste de liste de int """
+    H=[0 for i in range(h)]
+    max_mine_ligne=max_mine/h
+    print(max_mine_ligne)
+    nombre_mine_tot=0
+    for i in range(h):
+        L=[0 for _ in range(l)]
+        nombre_mine=0
+        for y in range(l):
+          x=randint(0,6)
+          if x==2 :
+             if(i,y)!=(Y,X) and nombre_mine < max_mine_ligne and nombre_mine_tot<max_mine:
+                L[y]='B'
+                nombre_mine+=1
+                nombre_mine_tot+=1
+        H[i]=L
+    H[Y][X]="A"
+    return H,nombre_mine_tot
+print(remplir(8,10,2,1,7))
