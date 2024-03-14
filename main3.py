d229de0a79a2b6fd159931c075f145d370f91093
moyenpingouin <127394110+moyenpingouin@users.noreply.github.com>
2024-03-14 09:22:31 +0100

    commit

--- main3.py
+++ main3.py
@@ -1,4 +1,4 @@
-#imports
+
 import fonctions
 from intialisation import *
 import classes
@@ -6,29 +6,30 @@ import pygame
 import PIL
 import sys
 
-#initialisation
-pygame.init()
-# Définir la taille de la fenêtre
-largeur, hauteur = 800, 600
-fenetre = pygame.display.set_mode((largeur, hauteur))
-fond = pygame.image.load(tableau_jeu.image)  
-fond = pygame.transform.scale(fond, (largeur, hauteur))
-# Définir la position initiale de l'image de fond
-fond_x = 0
-fond_y = 0
-
-
 #boucle globale
-run_glbl=True
-while run_glbl:
-    fenetre.blit(fond, (0, 0))
+def global_run():
+    #imports
+
+    #initialisation
+    pygame.init()
+    # Définir la taille de la fenêtre
+    largeur, hauteur = 800, 600
+    fenetre = pygame.display.set_mode((largeur, hauteur))
+    fond = pygame.image.load(tableau_jeu.image)  
+    fond = pygame.transform.scale(fond, (largeur, hauteur))
+    # Définir la position initiale de l'image de fond
+    fond_x = 0
+    fond_y = 0
+    run_glbl=True
+    while run_glbl:
+        fenetre.blit(fond, (0, 0))
 
 
 
 
-    for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+        for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        sys.exit()
 
-    pygame.display.flip()
+        pygame.display.flip()
